9f8e7913851e8b72f17d5b8e010c3a0589937799
new <new@new.com>
2020-12-27 16:12:19 +0300

    first commit

---
+++ schoolManagment/Person.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace schoolManagment
+{
+   public abstract class Person
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Email { get; set; }
+    }
+}
---
+++ schoolManagment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace schoolManagment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person newPerson = new Person();
+        }
+    }
+}
